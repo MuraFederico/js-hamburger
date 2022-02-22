8c224755329ca236f6ffcd34bd6df1166d190fb4
MuraFederico <federicomura98@gmail.com>
2022-02-22 15:24:36 +0100

    aggiunta chiusura on click

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let btnHam = document.querySelector('.header-right > a');
 let dropDown = document.querySelector('.hamburger-menu');
+let btnHidden = document.querySelector('.close');
 
-console.log(btnHam);
+
+// console.log(btnHam);
 
 
 btnHam.addEventListener('click', function() {
     dropDown.style.display = 'block';
 });
 
+btnHidden.addEventListener('click', function() {
+    dropDown.style.display = 'none';
+});

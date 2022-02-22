5fbf1e61519bccc3e220687157b2892bdbded82c
MuraFederico <federicomura98@gmail.com>
2022-02-22 15:14:46 +0100

    aggiunto event listener(non funzionante)

---
+++ main.js
@@ -0,0 +1,10 @@
+let btnHam = document.querySelector('a');
+let dropDown = document.querySelector('.hamburger-menu');
+
+// console.log(dropDown);
+
+
+btnHam.addEventListener('click', function() {
+    dropDown.style.display = 'block';
+});
+
